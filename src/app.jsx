89479cfca2f26c32746173e10c8b300f6e1daf48
monkey-bug <136386334+monkey-bug@users.noreply.github.com>
2025-10-19 10:23:34 -0500

    support single page app routing

--- src/app.jsx
+++ src/app.jsx
@@ -1,14 +1,34 @@
 import { render } from "preact";
+import { useContext } from "preact/hooks";
+import { LocationProvider, ErrorBoundary, Router, Route, useLocation } from "preact-iso";
 import "./app.css";
 
 import { Verify } from "./tools/verify.jsx";
 import { Menu } from "./menu.jsx";
+import { Settings, defaultSettings } from "./settings.js";
 
-function App() {
-	return <main>
-        <Menu />
-        <Verify />
-    </main>;
+function NotFound() {
+    const location = useLocation();
+    return <div>{location.path}</div>;
 }
 
-render(<App />, document.body);
+function AppRouter() {
+    const settings = useContext(Settings);
+    return <Router>
+        <Route path={`${settings.basepath}/verify`} component={Verify} />
+        <Route default component={NotFound} />
+    </Router>;
+}
+
+function App() {
+	return <LocationProvider>
+        <ErrorBoundary>
+            <Settings.Provider value={defaultSettings}>
+                <Menu />
+                <AppRouter />
+            </Settings.Provider>
+        </ErrorBoundary>
+    </LocationProvider>;
+}
+
+render(<App />, document.querySelector("main"));
